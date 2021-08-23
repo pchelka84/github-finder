cc9cb24bab054106d0da5887e8fc59803c071fc6
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-08-23 13:45:35 -0400

    enter and submit info to input field

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  // This is great when you have multiple input fields
+  // as you can reuse it
+  // otherwise, you can do
+  // this.setState({ text: e.taget.value })
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.text);
+  };
+
   render() {
     return (
-      <form className='form'>
-        <input type='text' name='text' placeholder='Searc Users...' />
+      <form onSubmit={this.onSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Searc Users...'
+          value={this.state.text}
+          onChange={this.onChange}
+        />
         <input
           type='submit'
           value='Search'
