11c08ec98b81b443c7afa738a2f65998d552b0f3
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-08-23 14:40:33 -0400

    create an alert when nothing is being submitted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import { Alert } from "./components/layout/Alert";
 import axios from "axios";
 import "./App.css";
 
@@ -10,6 +11,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // async componentDidMount() {
@@ -37,6 +39,13 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
     const { users, loading } = this.state;
 
@@ -44,10 +53,12 @@ class App extends Component {
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
