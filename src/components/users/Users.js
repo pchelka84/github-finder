993297cc4ecfacc2e673b09dbaf8fee01f856f58
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-08-23 10:04:25 -0400

    styling users layout

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -30,7 +30,7 @@ class Users extends Component {
 
   render() {
     return (
-      <div>
+      <div style={userStyle}>
         {this.state.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
@@ -38,5 +38,11 @@ class Users extends Component {
     );
   }
 }
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
+};
 
 export default Users;
