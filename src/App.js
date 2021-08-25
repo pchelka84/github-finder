08e38b627c69ed1e9847ef27f7e4301e2ffecd63
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-08-25 15:01:58 -0400

    create reducer and actions

--- src/App.js
+++ src/App.js
@@ -29,18 +29,6 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  // Search github users
-  const searchUsers = async (text) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.RECT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // Clear users from state
   const clearUsers = () => {
     setUsers([]);
@@ -91,7 +79,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
